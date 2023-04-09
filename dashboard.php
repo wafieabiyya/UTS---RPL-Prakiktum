ace2cc35009568230d5f04fa6d0abe3c932d137f
evlynngelica <evlyn.angelica@mhs.itenas.ac.id>
2023-04-09 22:28:27 +0700

    table dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+include('assets/server/connection.php');
+
+if (isset($_POST['cari'])) {
+    $keyword = $_POST['keyword'];
+    $query = "SELECT * FROM products WHERE id_product LIKE '%$keyword%' OR 
+        product_name LIKE '%$keyword%' OR product_category LIKE '%$keyword%' OR product_description LIKE '%$keyword%' ";
+} else {
+    $query = 'SELECT * FROM products';
+}
+
+$result = mysqli_query($conn, $query);
+
+if (!isset($_SESSION['logged_in'])) {
+    header('location: index.php');
+    exit;
+}
+
+if (isset($_GET['logout'])) {
+    if (isset($_SESSION['logged_in'])) {
+        unset($_SESSION['logged_in']);
+        unset($_SESSION['admin_email']);
+        header('location: index.php');
+        exit;
+    }
+}
+
+$kurs_dollar = 14940;
+
+function setRupiah($price)
+{
+    $result = "Rp" . number_format($price, 0, ',', '.');
+    return $result;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -63,6 +100,94 @@
                 <li><a href="#" class="active">Dashboard</a></li>
 
             </ul>
+            <div class="info-table">
+                <div class="card">
+                    <div class="head">
+                        <h2><?php
+                            $totalBarang = mysqli_query($conn, "SELECT COUNT(product_name) from products");
+                            $row = mysqli_fetch_assoc($totalBarang);
+                            $total = $row["COUNT(product_name)"];
+
+                            echo $total
+                            ?>
+                        </h2>
+                        <p>Jumlah Barang</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="head">
+                        <h2><?php
+                            $totalBarang = mysqli_query($conn, "SELECT SUM(product_quantity) from products");
+                            $row = mysqli_fetch_assoc($totalBarang);
+                            $total = $row["SUM(product_quantity)"];
+
+                            echo $total
+                            ?>
+                        </h2>
+                        <p>Total Quantity</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="head">
+                        <h2> <?php
+                                $totalAdmin = mysqli_query($conn, "SELECT COUNT(admin_name) FROM adm");
+                                $row = mysqli_fetch_assoc($totalAdmin);
+                                $total = $row["COUNT(admin_name)"];
+
+                                echo $total
+                                ?>
+                        </h2>
+                        <p>Jumlah Admin</p>
+                    </div>
+                </div>
+            </div>
+            <div class="table">
+                <div class="table-header">
+                    <p>Products Table</p>
+                    <form action="" method="POST">
+                        <div class="search">
+                            <input type="text" name="keyword" class="input-search" placeholder="search...">
+                            <button type="submit" name="cari"><i class="uil uil-search icon"></i></button>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="table-section">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Product</th>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = mysqli_fetch_assoc(($result))) { ?>
+                                <tr>
+                                    <td><?php echo $row['id_product'] ?></td>
+                                    <td><?php echo $row['product_name'] ?></td>
+                                    <td><?php echo $row['product_description'] ?></td>
+                                    <td><?php echo $row['product_category'] ?></td>
+                                    <td><?php echo $row['product_quantity'] ?></td>
+                                    <td><?php echo setRupiah(($row ['product_price'] * $kurs_dollar ))?></td>
+                                    <td>
+                                        <a class="bx bx-edit" href="editProduct.php?id_product=<?php echo $row['id_product']; ?>" role="button" onclick="return confirm ('Are you sure want to edit this data?')">
+                                        
+                                        </a>
+                                        <a class="bx bx-trash" href="deleteAction.php?id_product=<?php echo $row['id_product']; ?>" role="button" onclick="return confirm('Are you sure want to delete this data?')">
+                                        
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </main>
         <!-- --------------------MAIN END ------------------------ -->
 
